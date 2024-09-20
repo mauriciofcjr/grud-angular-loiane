7e55f86c96bca53d1055e19da39f2ac7efe7363b
Mauricio Chaves <feira.nova@gmail.com>
2024-09-20 11:37:46 -0300

    Css do material table, criação de modulo exclusivo para o Material

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrl: './courses.component.css'
+  styleUrl: './courses.component.css',
 })
 export class CoursesComponent {
-
+  displayedColumns: string[] = ['name', 'category'];
+  courses: Course[] = [
+    { _id: '1', name: 'SpringBoot', category: 'Back-end' },
+    { _id: '2', name: 'Angular', category: 'Front-end' },
+  ];
 }
